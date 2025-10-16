a8e64aa14519dee8320e18ac3c4c2f8ad794ba8c
Muhammad Adil <madil@esoftbay.com>
2025-10-16 04:36:36 +0100

    Initial commit
    Generated by react-native@0.81.4

---
+++ android/app/src/main/java/com/taskmate_app_using_genai/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.taskmate_app_using_genai
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "TaskMate_App_Using_GenAI"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
