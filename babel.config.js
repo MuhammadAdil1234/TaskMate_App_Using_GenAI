cac432a878067d91f0c5dc04da04813485a014e7
MuhammadAdil1234 <muhammadadil0994@gmail.com>
2025-10-28 18:14:37 +0500

    Project Structure setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
