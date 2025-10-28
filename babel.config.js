ce7a5fd85c18ac146d32e69383852abcd4fa5dc3
MuhammadAdil1234 <muhammadadil0994@gmail.com>
2025-10-28 18:33:35 +0500

    babel update

--- babel.config.js
+++ babel.config.js
@@ -6,15 +6,11 @@ module.exports = {
       {
         root: ['./src'],
         alias: {
-          '@api': './src/api',
           '@assets': './src/assets',
           '@components': './src/components',
           '@constants': './src/constants',
-          '@hooks': './src/hooks',
           '@navigation': './src/navigation',
           '@screens': './src/screens',
-          '@services': './src/services',
-          '@store': './src/store',
           '@utils': './src/utils',
         },
       },
